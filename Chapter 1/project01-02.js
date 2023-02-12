616db7cce5fedf84f2e954f593ad7188f6730d9b
Ulfany Guimel <102819919+ulfanyguimel@users.noreply.github.com>
2023-02-12 16:44:22 -0500

    Add files via upload

---
+++ Chapter 1/project01-02.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Ulfany Furcal
+    Date:   1/26/2023
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+service1Name = "Basic";
+service2Name = "Express";
+service3Name = "Extreme";
+service4Name = "Ultimate";
+service1Speed = "0 Mbps";
+service2Speed = "100 Mbps";
+service3Speed = "500 Mbps";
+service4Speed = "1 Mbps";
+
+
---
+++ Chapter 1/project01-04.js
@@ -0,0 +1,13 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Ulfany Furcal   
+    Date:   1/28/2023
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+let homeStreet = "1 Main St.", homeCity = "Sicilia", homeState = "MA", homeCode = "02103", workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104";
+
